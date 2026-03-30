7402f9d1e78504c8dee285b641481ef3f58178c3
Dominikapolchlopek <111249034+Dominikapolchlopek@users.noreply.github.com>
2026-03-30 22:05:02 +0200

    v1.0.0
    Fetches currency exchange rates from the Fixer API and saves them to rates.json.

---
+++ update_rates.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import os
+
+# Pobieranie klucza z bezpiecznych ustawień GitHuba
+API_KEY = os.getenv('FIXER_API_KEY')
+URL = f"http://data.fixer.io/api/latest?access_key={API_KEY}"
+
+def fetch_data():
+    try:
+        response = requests.get(URL)
+        data = response.json()
+        
+        if data.get('success'):
+            # Zapisujemy do pliku rates.json
+            with open('rates.json', 'w') as f:
+                json.dump(data, f, indent=4)
+            print("Success.")
+        else:
+            print(f"Błąd API: {data.get('error')}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    fetch_data()
